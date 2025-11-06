7bf892b09eb0681092c9a182a838eb7f9c76bb18
RL-55 <rickyl55@nycstudents.net>
2025-11-06 09:23:28 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+
+import requests
+
+def getPoke(poke):
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    data = response.json()
+    return {
+        "name": data["name"],
+        "abilities":[a["ability"]["name"] for a in data["abilities"]],
+        "height": data["height"],
+        "weight": data["weight"],
+        "types": [t["type"]["name"] for t in data["types"]]
+    }
+
+pokemon = getPoke("garchomp")
+for key, value in pokemon.items():
+    print(key,":",value)
