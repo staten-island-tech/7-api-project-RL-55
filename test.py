90f8e3a567ed5ea0df56fc29f5b94c1db9339b96
RL-55 <rickyl55@nycstudents.net>
2025-11-17 09:25:49 -0500

    Update test.py

--- test.py
+++ test.py
@@ -8,8 +8,6 @@ prompttext = tk.Label(window, text="Search for:",font=("Arial", 14))
 prompttext.pack(pady=10)
 entrybox = tk.Entry(window, font=("Arial", 14), width=40)
 entrybox.pack(pady=5)
-list=[]
-list2=[]
 def entry(botw):
     response = requests.get(f"https://botw-compendium.herokuapp.com/api/v3/compendium/entry/{botw.lower()}")
     if response.status_code != 200:
@@ -21,21 +19,17 @@ def entry(botw):
         "id":data["data"]["id"],
         "category": data["data"]["category"],
         "location": data["data"]["common_locations"],
-        "drops": data["data"]["drops"]
+        "drops": data["data"]["drops"],
+        "image":data["data"]["image"]
         }
 def entryget():
     output = (entrybox.get()).lower()
     entry2=entry(output)
     for key, value in entry2.items():
-            print(key,":",value)
-    outputtext.config(text= f"{key[0]} : {value[0]}")
-
-
+        x=(key,":",value)
+        outputtext = tk.Label(window, text=x, font=("Arial", 14),fg="blue",wraplength=570, justify="left")
+        outputtext.pack(pady=2)
 
 confirm_button = tk.Button(window, text="Search",font=("Arial", 14),command=entryget)
 confirm_button.pack(pady=10)
-
-outputtext = tk.Label(window, text="w", font=("Arial", 14),fg="blue")
-outputtext.pack(pady=5)
-
 window.mainloop()
