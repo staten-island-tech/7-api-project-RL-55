158a81cbf4780bd1a5eee7d9ff8045d2b75128c9
RL-55 <rickyl55@nycstudents.net>
2025-11-20 09:09:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,8 @@
 def password(Email,Password):
-    n=a=b=False
-    if type(Email)==str and type(Password)==str:
-        if "@" in Email:
-            for i in Password:
-                if i.isupper():
-                    a=True
-                elif i.isdigit():
-                    b=True
-                n+=1
-            if a==b==True and n>=8:
-                print("Account made",Email,":",Password)
-            else:
-                if a==0:
-                    print("Password doesn't include a capital.")
-                elif b==0:
-                    print("Password doesn't include a number.")
-                else:
-                    print("Password too short.")
-        else:
-            print("Invalid Email type.")
-    else:
-        print("Email not a string.")
-password("rickyl55@nycstudents.net","NathanC124")
+    if type(Email)!=str and type(Password)!=str: return("Email or password not a string.")
+    if "@" not in Email: return("Invalid Email type.")
+    if not any (i.isupper for i in Password): return("Password doesn't include a capital.")
+    if not any (i.isdigit for i in Password): return("Password doesn't include a number.")
+    if not len(Password)>=8: return("Password too short.")
+    return("Account made",f"Your Email:{Email} and Password:{Password}")
+print(password("rickyl55@nycstudents.net","NathanC"))
