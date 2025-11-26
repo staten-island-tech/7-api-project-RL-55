2d08ede566d29df05bb55aecdf372f77a1464e35
RL-55 <rickyl55@nycstudents.net>
2025-11-26 09:16:52 -0500

    Update gamble.py

--- gamble.py
+++ gamble.py
@@ -9,11 +9,11 @@ def gamble():
     c=random.randint(1,9)
     x=a,b,c
     if a==b==c:
-        outputtext = tk.Label(window, text=x, font=("Courier New", 14),fg="green",wraplength=570, justify="left")
-        outputtext.pack(pady=2)
+        outputtext.config(text=x, font=("Courier New", 14),fg="green")
     else:
-        outputtext = tk.Label(window, text=x, font=("Courier New", 14),fg="tomato",wraplength=570, justify="left")
-        outputtext.pack(pady=2)
+        outputtext.config(text=x, font=("Courier New", 14),fg="tomato")
 confirm_button=tk.Button(window, text=" Roll ",font=("Georgia", 14),command=gamble)
-confirm_button.pack(pady=2)
+confirm_button.pack(ipadx=25)
+outputtext = tk.Label(window, text="0,0,0", font=("Courier New", 14),fg="green",wraplength=570, justify="left")
+outputtext.pack(pady=2)
 window.mainloop()
